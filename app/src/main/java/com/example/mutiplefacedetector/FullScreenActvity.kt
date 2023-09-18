04b8f97ee5529be629eaaa486c978567728daebb
azzadpandit1 <azzad.pandit@aurosociety.org>
2023-09-18 14:53:29 +0530

    service code

--- app/src/main/java/com/example/mutiplefacedetector/FullScreenActvity.kt
+++ app/src/main/java/com/example/mutiplefacedetector/FullScreenActvity.kt
@@ -1,15 +1,16 @@
 package com.example.mutiplefacedetector
 
 import android.annotation.SuppressLint
-import android.app.StatusBarManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbConstants
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mutiplefacedetector.databinding.ActivityFullScreenActvityBinding
@@ -19,53 +20,76 @@ import java.lang.reflect.Method
 class FullScreenActvity : AppCompatActivity() {
     private val binding by lazy { ActivityFullScreenActvityBinding.inflate(layoutInflater) }
 
-    private lateinit var cameraDetector: CameraDetector
+    /*private lateinit var cameraDetector: CameraDetector
+
+    private val TAG = "MainActivity"
+    private lateinit var notificationReceiver: BroadcastReceiver*/
+
 
     private lateinit var usbManager: UsbManager
+    private lateinit var usbReceiver: BroadcastReceiver
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-       /* val usbManager = getSystemService(USB_SERVICE) as UsbManager
-        cameraDetector = CameraDetector(usbManager)*/
-
         usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
 
-        val usbCameraList: List<UsbDevice> = getConnectedUsbCameras()
-
-        binding.textViewDevices.setOnClickListener {
-
-            getConnectedUsbCameras()
-
-            binding.textViewDevices.text = "Device : "+usbCameraList.size
-
-        }
-
-
-    }
-    private fun getConnectedUsbCameras(): List<UsbDevice> {
-        val usbCameraList: MutableList<UsbDevice> = mutableListOf()
-
-        val usbDevices: HashMap<String, UsbDevice> = usbManager.deviceList
-
-        for (device in usbDevices.values) {
-            if (device.deviceClass == UsbConstants.USB_CLASS_VIDEO) {
-                usbCameraList.add(device)
+        // Register a broadcast receiver to listen for USB device connection and disconnection events
+        usbReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+                if (UsbManager.ACTION_USB_DEVICE_ATTACHED == action) {
+                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                    device?.let {
+                        // A USB device is attached, do something with it
+                        val deviceName = device.deviceName
+                        val vendorId = device.vendorId
+                        val productId = device.productId
+                        // Display the device information
+                        println("USB Device Attached:")
+                        println("Device Name: $deviceName")
+                        println("Vendor ID: $vendorId")
+                        println("Product ID: $productId")
+                    }
+                } else if (UsbManager.ACTION_USB_DEVICE_DETACHED == action) {
+                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                    device?.let {
+                        // A USB device is detached, do something with it
+                        val deviceName = device.deviceName
+                        val vendorId = device.vendorId
+                        val productId = device.productId
+                        // Display the device information
+                        println("USB Device Detached:")
+                        println("Device Name: $deviceName")
+                        println("Vendor ID: $vendorId")
+                        println("Product ID: $productId")
+                    }
+                }
             }
         }
 
-        return usbCameraList
-    }
-    override fun onDestroy() {
-        super.onDestroy()
+        // Register the broadcast receiver to listen for USB device events
+        val filter = IntentFilter()
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
+        registerReceiver(usbReceiver, filter)
+
+
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Unregister the broadcast receiver when the activity is destroyed
+        unregisterReceiver(usbReceiver)
+    }
+
     override fun onResume() {
         super.onResume()
-        //cameraDetector.detectCameras()
-        //binding.textViewDevices.text = "Device : "+cameraDetector.detectCameras().size
+      //  cameraDetector.detectCameras()
+//        binding.textViewDevices.text = "Device : "+cameraDetector.detectCameras().size
     }
 
     override fun onStop() {
@@ -76,6 +100,7 @@ class FullScreenActvity : AppCompatActivity() {
         super.onWindowFocusChanged(hasFocus)
         if (!hasFocus) {
             Toast.makeText(this,"sorry you can't open notifications on quiz time ",Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this,"NOTIFICATION BAR IS DOWN",Toast.LENGTH_SHORT).show()
             // NOTIFICATION BAR IS DOWN...DO STUFF
             setExpandNotificationDrawer(this,false) // close notification panel
         }
